3c457cd4d00506e0c6a5efb68a0a43984de0d6e2
KarolineKlan <karolineklanhansen@gmail.com>
2025-10-27 11:15:41 +0100

    init cookiecutter project

---
+++ src/deep_proj/train.py
@@ -0,0 +1,10 @@
+from deep_proj.model import Model
+from deep_proj.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
